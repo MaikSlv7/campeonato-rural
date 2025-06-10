852817b17d989171e74faed8a92c9d5dc067fe4c
Maik Silva <silvamaik52@gmail.com>
2025-06-10 11:12:38 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,12 @@
-
 const firebaseConfig = {
-  databaseURL: "https://campeonato-rural-default-rtdb.firebaseio.com/"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://campeonato-rural-default-rtdb.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
